564411c97f29935f8d9fb3fe07ce47ce52b9bdcb
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-10-11 11:18:34 +0530

    lexical analyser using C

---
+++ 9pgm/input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=5,b=10,c_b, big;  //This is a single line comment
+        c_b = b/a;
+        if(a>b)
+            big=a;
+    while(a>=b&&c_b<9)
+    {
+    a=2&c_b;
+    }
+    a/=b;
+    printf("Hello World");
+}
